7f272deb3ae50509f2e54b2f4252c40827ebc369
Kaartik Arora <kaartikarora0001@gmail.com>
2026-05-24 18:26:32 +0530

    designed home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+
+      {/* map over an array of diff. kinds of resumes */}
+      {/* now instead of creating this architecture here we shift this to constants */}
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
---
+++ types/index.d.ts
@@ -0,0 +1,53 @@
+// as soon as this created now it will be auto recognised by all the files
+interface Resume {
+    id: string;
+    companyName?: string;
+    jobTitle?: string;
+    imagePath: string;
+    resumePath: string;
+    feedback: Feedback;
+}
+
+// ai generated feedback of the resume
+interface Feedback {
+    overallScore: number;
+    ATS: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+        }[];
+    };
+    toneAndStyle: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+    content: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+    structure: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+    skills: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+}
